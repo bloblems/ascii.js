26bf18f13330ed6eeee3f10c24205e6e953f6ad9
cactusfluo <cactus@ntymail.com>
2020-02-24 09:50:29 +0100

    [+]particles: trying to get a list of character sort by density

--- tests/particles/main.js
+++ tests/particles/main.js
@@ -31,21 +31,18 @@ function	draw() {
 
 	clear();
 	new_particles = [];
+	/// LOOP THROUGH PARTICLES
 	for (i = 0; i < particles.length; ++i) {
 		particle = particles[i];
+		/// DRAW PARTICLE
 		ascii[particle.y][particle.x] = characters[particle.char];
-		particle.char = Math.max(particle.char - 1, 0);
-		particle.y += 1;
-		if (particle.y < canvas_height) {
+		particle.char -= 1;
+		if (particle.char >= 0) {
 			new_particles.push(particle);
 		}
 	}
+	/// ADD NEW PARTICLE
 	new_particles.push({"x": mouse_x, "y": mouse_y, "char": characters.length - 1});
-	if (mouse_x < canvas_width - 1) {
-		new_particles.push({"x": mouse_x + 1, "y": mouse_y, "char": characters.length - 1});
-	}
-	if (mouse_x > 0) {
-		new_particles.push({"x": mouse_x - 1, "y": mouse_y, "char": characters.lenth - 1});
-	}
+	/// UPDATE PARTICLE LIST
 	particles = new_particles;
 }
