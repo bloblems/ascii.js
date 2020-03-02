4092f1dcaf19bae370abc2f5c199e33e32bc9a7c
cactusfluo <cactus@ntymail.com>
2020-03-02 15:47:31 +0100

    [~]particles/*: got a nice sketch with particles

--- tests/particles/main.js
+++ tests/particles/main.js
@@ -12,45 +12,102 @@ window.requestAnimationFrame = window.requestAnimationFrame
 ////////////////////////////////////////////////////////////////////////////////
 
 const	characters = ".,-*:;!ioea#IOXHM8&@";
+const	NUM_PARTICLES	= 10;
 
-let		prev_x, prev_y;
+let		followers;
 let		particles;
+
+////////////////////////////////////////////////////////////////////////////////
+/// CLASS
+////////////////////////////////////////////////////////////////////////////////
+
+class		Follower {
+	constructor() {
+		let	i;
+
+		this.x = floor(random(canvas_width));
+		this.y = floor(random(canvas_height));
+		this.acc_x = 0;
+		this.acc_y = 0;
+	}
+
+	update() {
+		let	i;
+
+		if (this.x < mouse_x) {
+			this.acc_x += 0.1;
+		} else {
+			this.acc_x -= 0.1;
+		}
+		if (this.y < mouse_y) {
+			this.acc_y += 0.1;
+		} else {
+			this.acc_y -= 0.1;
+		}
+		this.acc_x *= 0.99;
+		this.acc_y *= 0.99;
+		this.x += this.acc_x;
+		this.y += this.acc_y;
+		particles.push(new Particle(floor(this.x), floor(this.y)));
+	}
+
+	draw() {
+		if (this.x >= 0 && this.x < canvas_width && this.y >= 0 && this.y < canvas_height) {
+			ascii[floor(this.y)][floor(this.x)] = '@';
+		}
+	}
+}
+
+class	Particle {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.c = characters.length - 1;
+	}
+
+	update() {
+		this.c -= 1;
+		return (this.c >= 0);
+	}
+
+	draw() {
+		if (this.x >= 0 && this.x < canvas_width && this.y >= 0 && this.y < canvas_height) {
+			ascii[this.y][this.x] = characters[this.c];
+		}
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
 
 function	setup() {
+	let		i;
+
 	create_canvas();
+	mouse_x = canvas_width / 2;
+	mouse_y = canvas_height / 2;
 	particles = [];
-	prev_x = prev_y = 0;
+	followers = [];
+	for (i = 0; i < NUM_PARTICLES; ++i) {
+		followers.push(new Follower());
+	}
 }
 
 function	draw() {
-	let		new_particles;
-	let		particle;
 	let		i;
+	let		new_particles;
 
 	clear();
+	for (i = 0; i < NUM_PARTICLES; ++i) {
+		followers[i].update();
+		followers[i].draw();
+	}
 	new_particles = [];
-	/// LOOP THROUGH PARTICLES
 	for (i = 0; i < particles.length; ++i) {
-		particle = particles[i];
-		/// DRAW PARTICLE
-		ascii[particle.y][particle.x] = characters[particle.char];
-		particle.char -= 1;
-		if (particle.char >= 0) {
-			new_particles.push(particle);
+		if (particles[i].update() == true) {
+			new_particles.push(particles[i]);
+			particles[i].draw();
 		}
 	}
-	/// ADD NEW PARTICLES
-	line_func(prev_x, prev_y, mouse_x, mouse_y, function (x, y, in_frame) {
-		if (in_frame == true) {
-			new_particles.push({"x": x, "y": y, "char": characters.length - 1});
-		}
-	});
-	prev_x = mouse_x;
-	prev_y = mouse_y;
-	/// UPDATE PARTICLE LIST
-	particles = new_particles;
 }
